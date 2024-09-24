694e1ab55c06551612c38de62ba4e4c43a6c8d99
drlearn <hadee_16@yahoo.com>
2024-09-25 01:25:47 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = 'cc9be30a-cdfb-493f-8945-a1d167ab6453';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com'];
